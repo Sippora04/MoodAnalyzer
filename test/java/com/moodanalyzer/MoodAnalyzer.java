1c8333050a54f1302dc5d5e174ac6c78b03038cb
Sippora04 <sippora.toppo95@gmail.com>
2020-10-28 22:53:37 +0530

    Added: Null Exception

--- test/java/com/moodanalyzer/MoodAnalyzer.java
+++ test/java/com/moodanalyzer/MoodAnalyzer.java
@@ -1,18 +1,19 @@
 package com.moodanalyzer;
 
 public class MoodAnalyzer {
-
-	//TC1.1 : Given "I am in Sad Mood" message Should Return SAD
-	//TC1.2 : Given "I am in Any Mood" message Should Return HAPPY
 	private String message; 
 
 	public String analyzeMood(String mood) {
-		if(message.contains("sad"))
-			return "SAD";
-		else if(message.contains("any"))
+		try {
+			if(message.contains("sad")) 
+				return "SAD";
+			else if(message.contains("any"))
+					return "HAPPY";
+		} catch (NullPointerException e) {
 			return "HAPPY";
-		return mood;
 		}
+		return mood;
+	}
 		//Default Constructor
 		public MoodAnalyzer() {
 		
--- test/java/com/moodanalyzer/MoodAnalyzerTest.java
+++ test/java/com/moodanalyzer/MoodAnalyzerTest.java
@@ -7,17 +7,23 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
 
 	@Test
-    public void givenMeassage_WhenValid_ThenReturnMood() {
+    public void givenMeassage_WhenSad_ThenReturnMood() {
         MoodAnalyzer moodAnalyzeObj = new MoodAnalyzer("I am in sad mood");
         String mood=moodAnalyzeObj.analyzeMood("");
         assertEquals("SAD", mood);
     }
 	
-    //TEST CASE TO CHECK HAPPY MOOD
-    @Test
-    public void givenMeassage_WhenInvalid_ThenReturnMood() {
+	@Test
+    public void givenMeassage_WhenNotSad_ThenReturnMood() {
         MoodAnalyzer moodAnalyzeObj = new MoodAnalyzer("I am in any mood");
         String mood = moodAnalyzeObj.analyzeMood("");
         assertEquals("HAPPY", mood);
     }
+	
+	 @Test
+	 public void givenNull_WhenInvalid_ShouldReturnHappy() {
+		 MoodAnalyzer moodAnalyzeObj = new MoodAnalyzer();
+	     String mood = moodAnalyzeObj.analyzeMood("");
+	     assertEquals("HAPPY", mood);
+	    }
 }
