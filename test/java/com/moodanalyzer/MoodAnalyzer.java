9b1e7f50b1a2e32eaf338f3bb4f15b7b91da00a3
Sippora04 <sippora.toppo95@gmail.com>
2020-10-28 21:33:15 +0530

    Refactored: message in constructor

--- test/java/com/moodanalyzer/MoodAnalyzer.java
+++ test/java/com/moodanalyzer/MoodAnalyzer.java
@@ -5,7 +5,7 @@ public class MoodAnalyzer {
 	//TC1.1 : Given "I am in Sad Mood" message Should Return SAD
 	//TC1.2 : Given "I am in Any Mood" message Should Return HAPPY
 	private String message; 
-	System.out.println("Welcome to Mood Analyzer Problem");
+
 	public String analyzeMood(String mood) {
 		if(message.contains("sad"))
 			return "SAD";
